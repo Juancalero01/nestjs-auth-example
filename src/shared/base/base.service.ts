62e72ba2f2960bfb2b1afce345f9a212949770b0
juancalero <marcelocalero5@gmail.com>
2024-09-19 10:31:43 -0300

    init

--- src/shared/base/base.service.ts
+++ src/shared/base/base.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -9,22 +13,42 @@ export abstract class BaseService<T> {
     this.repository = repository;
   }
 
-  async findAll(): Promise<T[]> {
-    return await this.repository.find();
+  async findAll(options?: any): Promise<T[]> {
+    try {
+      const entities = await this.repository.find(options);
+      if (!entities.length) throw new NotFoundException('No entities found');
+      return entities;
+    } catch (error) {
+      throw new BadRequestException('Error fetching data', error.message);
+    }
   }
 
   async findOne(id: number): Promise<T> {
-    return await this.repository.findOne({ where: { id } as any });
+    try {
+      const entity = await this.repository.findOne({ where: { id } as any });
+      if (!entity)
+        throw new NotFoundException(`Entity with ID ${id} not found`);
+      return entity;
+    } catch (error) {
+      throw new BadRequestException('Error fetching entity', error.message);
+    }
   }
 
   async create(entity: T): Promise<T> {
-    return await this.repository.save(entity);
+    try {
+      return await this.repository.save(entity);
+    } catch (error) {
+      throw new BadRequestException('Error creating entity', error.message);
+    }
   }
 
-  async update(id: number, entity: T): Promise<T> {
-    const existingEntity = await this.repository.findOne({
-      where: { id } as any,
-    });
-    return await this.repository.save({ ...existingEntity, ...entity });
+  async update(id: number, entity: Partial<T>): Promise<T> {
+    try {
+      const existingEntity = await this.findOne(id);
+      const updatedEntity = Object.assign(existingEntity, entity);
+      return await this.repository.save(updatedEntity);
+    } catch (error) {
+      throw new BadRequestException('Error updating entity', error.message);
+    }
   }
 }
