e295d6edd2a66ad5cdc714dadbd926e2753d85ea
juancalero <marcelocalero5@gmail.com>
2024-09-19 11:59:58 -0300

    init

---
+++ src/health/health.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, HttpCode } from '@nestjs/common';
+
+@Controller('health')
+export class HealthController {
+  constructor() {}
+
+  @Get()
+  @HttpCode(200)
+  run() {
+    return { status: 'ok' };
+  }
+}
