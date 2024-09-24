65238d46b20e5d3b7848c4141fb849e543ed97a3
juancalero <marcelocalero5@gmail.com>
2024-09-23 23:25:22 -0300

    init

--- src/policy/policies.guard.ts
+++ src/policy/policies.guard.ts
@@ -1,26 +1,38 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  Inject,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { CaslAbilityFactory } from '../ability/ability.factory';
-import { CHECK_POLICIES_KEY } from './policies.decorator';
-import { PolicyHandler } from './policy-handler.interface';
-import { AppAbility } from '../ability/ability.factory';
 import { JwtService } from '@nestjs/jwt';
+import { IS_PUBLIC_KEY } from '../utils/decorators/public.decorator';
+import { CHECK_POLICIES_KEY } from './policies.decorator';
+import { AbilityFactory } from '../ability/ability.factory';
+import { PolicyHandler } from './policy-handler.interface';
+import { AppAbility } from '../ability/ability.type';
 
 @Injectable()
 export class PoliciesGuard implements CanActivate {
   constructor(
     private reflector: Reflector,
-    private caslAbilityFactory: CaslAbilityFactory,
+    private caslAbilityFactory: AbilityFactory,
     private jwtService: JwtService,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) return true;
+
     const request = context.switchToHttp().getRequest();
     const token = request.headers.authorization?.split(' ')[1];
     if (!token) return false;
 
     const user = this.jwtService.decode(token) as any;
-
     const ability = await this.caslAbilityFactory.createForUser(user);
 
     const policyHandlers =
@@ -28,6 +40,8 @@ export class PoliciesGuard implements CanActivate {
         CHECK_POLICIES_KEY,
         context.getHandler(),
       ) || [];
+
+    if (policyHandlers.length === 0) return false;
 
     return policyHandlers.every((handler) =>
       this.execPolicyHandler(handler, ability),
