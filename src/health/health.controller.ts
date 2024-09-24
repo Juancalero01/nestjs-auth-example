cb427383624fca8f6110ada8318f6affe0e8e49c
juancalero <marcelocalero5@gmail.com>
2024-09-24 12:52:43 -0300

    init

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,11 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
 import { Public } from '../utils/decorators/public.decorator';
 
 @Controller('health')
 export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private db: TypeOrmHealthIndicator,
+  ) {}
+
   @Public()
   @Get('/')
-  run() {
-    return { status: 'ok' };
+  @HealthCheck()
+  readiness() {
+    return this.health.check([
+      async () => this.db.pingCheck('database', { timeout: 300 }),
+    ]);
   }
 }
