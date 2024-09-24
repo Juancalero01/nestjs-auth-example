5c3d3ebbef92823a8ec9a75e75d21e6a831169ad
juancalero <marcelocalero5@gmail.com>
2024-09-23 23:23:49 -0300

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_GUARD } from '@nestjs/core';
 import { DatabaseConfig } from './config/database.config';
 import { HealthModule } from './health/health.module';
 import { AuthModule } from './auth/auth.module';
@@ -8,6 +9,9 @@ import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
 import { PermissionModule } from './permission/permission.module';
 import { AbilityModule } from './ability/ability.module';
+import { JwtAuthGuard } from './auth/jwt.guard';
+import { PoliciesGuard } from './policy/policies.guard';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -24,6 +28,17 @@ import { AbilityModule } from './ability/ability.module';
     RoleModule,
     PermissionModule,
     AbilityModule,
+  ],
+  providers: [
+    JwtService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: PoliciesGuard,
+    },
   ],
 })
 export class AppModule {}
