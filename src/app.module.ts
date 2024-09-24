051cd42f42431755aaada8d4b2583bb519cc06b1
juancalero <marcelocalero5@gmail.com>
2024-09-23 23:23:55 -0300

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_GUARD } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 import { DatabaseConfig } from './config/database.config';
+import { JwtAuthGuard } from './auth/jwt.guard';
+import { PoliciesGuard } from './policy/policies.guard';
 import { HealthModule } from './health/health.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
 import { PermissionModule } from './permission/permission.module';
 import { AbilityModule } from './ability/ability.module';
-import { JwtAuthGuard } from './auth/jwt.guard';
-import { PoliciesGuard } from './policy/policies.guard';
-import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
